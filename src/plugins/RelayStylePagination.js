4fff29b3bbffd8df91cfc57f631e20b9170cd27a
Sayan Dey <mr.sayandey18@outlook.com>
2025-02-17 00:45:23 +0530

    Implement GraphQL pagination

---
+++ src/plugins/RelayStylePagination.js
@@ -0,0 +1,27 @@
+import { relayStylePagination } from '@apollo/client/utilities';
+
+export class RelayStylePagination {
+    apply(hooks) {
+        const { addFilter } = hooks;
+
+        addFilter('apolloClientInMemoryCacheOptions', 'faust', (options) => {
+            return {
+                ...options,
+                typePolicies: {
+                    ...options.typePolicies,
+                    RootQuery: {
+                        ...options.typePolicies.RootQuery,
+                        fields: {
+                            posts: relayStylePagination(),
+                        },
+                    },
+                    ContentType: {
+                        fields: {
+                            contentNodes: relayStylePagination(),
+                        },
+                    },
+                },
+            };
+        });
+    }
+}
