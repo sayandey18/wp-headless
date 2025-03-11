4c87ee29743a68710cbd1c40f42bc3a4dc7be20b
Sayan Dey <mr.sayandey18@outlook.com>
2025-03-12 00:59:32 +0530

    Remove port from image source

--- next.config.mjs
+++ next.config.mjs
@@ -12,55 +12,46 @@ const nextConfig = {
             {
                 protocol: 'http',
                 hostname: 'localhost',
-                port: '',
                 pathname: '/**'
             },
             {
                 protocol: 'https',
                 hostname: getWpHostname(),
-                port: '',
                 pathname: '/**'
             },
             {
                 protocol: 'https',
                 hostname: '0.gravatar.com',
-                port: '',
                 pathname: '/**'
             },
             {
                 protocol: 'https',
                 hostname: '1.gravatar.com',
-                port: '',
                 pathname: '/**'
             },
             {
                 protocol: 'https',
                 hostname: '2.gravatar.com',
-                port: '',
                 pathname: '/**'
             },
             {
                 protocol: 'https',
                 hostname: '3.gravatar.com',
-                port: '',
                 pathname: '/**'
             },
             {
                 protocol: 'https',
                 hostname: 'secure.gravatar.com',
-                port: '',
                 pathname: '/**'
             },
             {
                 protocol: 'https',
                 hostname: 'images.unsplash.com',
-                port: '',
                 pathname: '/**'
             },
             {
                 protocol: 'https',
                 hostname: 'cdn.prod.website-files.com',
-                port: '',
                 pathname: '/**'
             }
         ]
